c9a9d3b1910cd3c5eebd1610ad674e0e4d81cf6c
Ben Hsieh <Ben.T.Hsieh@gmail.com>
2016-03-05 15:54:11 -0800

    add dashboard cards and router to gallery

---
+++ src/components/menu/menu.ts
@@ -0,0 +1,14 @@
+import {Component} from "angular2/core";
+import {Router} from 'angular2/router';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+@Component({
+    selector: 'menu',
+    template: require('./menu.html'),
+    directives: [ROUTER_DIRECTIVES]
+    
+})
+export class MenuComponent {
+    
+    }
+}
