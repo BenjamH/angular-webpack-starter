9222955f4f9dc8aec18982a001f40cc041897cea
Ben Hsieh <Ben.T.Hsieh@gmail.com>
2016-03-05 15:53:41 -0800

    add dashboard cards and router to gallery

--- src/routes/app-shell/app-shell.ts
+++ src/routes/app-shell/app-shell.ts
@@ -1,12 +1,14 @@
 import {Component} from "angular2/core";
-import {RouteConfig, RouterOutlet} from "angular2/router";
+import {RouteConfig, RouterOutlet, RouterLink, ROUTER_PROVIDERS} from "angular2/router";
 import {HomeComponent} from "../home/home";
 import {GalleryRouteComponent} from "../gallery/gallery";
+import {MenuComponent} from "../../components/menu/menu";
 
 @Component({
     selector: 'app-shell',
     template: require('./app-shell.html'),
-    directives: [RouterOutlet]
+    directives: [RouterOutlet, RouterLink, MenuComponent],
+    providers: [ROUTER_PROVIDERS]
 })
 @RouteConfig([
     { name: 'Home', path:'/', component: HomeComponent, useAsDefault: true},
