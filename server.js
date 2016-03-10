c04a4ea43612f018390d85e2aae2d5eed8bf476a
Ben Hsieh <Ben.T.Hsieh@gmail.com>
2016-03-10 03:44:03 -0800

    config server file

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var webpack = require('webpack');
 var app = express();
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
-var static_path = path.join(__dirname, 'public');
+var static_path = path.join(__dirname);
 
 app.use(express.static(static_path))
     .get('/', function (req, res) {
         res.sendFile('index.html', {
             root: static_path
         });
-    }).listen(process.env.PORT || 8080, function (err) {
+    }).listen(3000, function (err) {
     if (err) { console.log(err) };
-    console.log('Listening at localhost:8080');
+    console.log('Listening at localhost:3000');
 });
 
 if (isDevelopment) {
@@ -23,8 +23,8 @@ if (isDevelopment) {
     new WebpackDevServer(webpack(config), {
         publicPath: config.output.publicPath,
         hot: true
-    }).listen(3000, 'localhost', function (err, result) {
+    }).listen(process.env.PORT || 8080, 'localhost', function (err, result) {
         if (err) { console.log(err) }
-        console.log('Listening at localhost:3000');
+        console.log('Listening at localhost:8080');
     });
 }
